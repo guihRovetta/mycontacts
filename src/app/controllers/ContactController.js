a465d1748c01514a40c417a402b3b81865c51e4d
guihRovetta <guilherme020213@hotmail.com>
2020-08-14 08:58:47 -0300

    :sparkles: added update route

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -31,7 +31,7 @@ class ContactController {
     if (contactExists) {
       return response
         .status(400)
-        .json({ error: 'This e-mail is already been taken' });
+        .json({ error: 'This e-mail is already in use' });
     }
 
     const contact = await ContactsRepository.create({
@@ -44,7 +44,37 @@ class ContactController {
     response.json(contact);
   }
 
-  update() {}
+  async update(request, response) {
+    const { id } = request.params;
+    const { name, email, phone, category_id } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response
+        .status(400)
+        .json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    response.json(contact);
+  }
 
   async delete(request, response) {
     const { id } = request.params;
